b6da73afa3cc16d473a349693d44eaa8e362f70b
yokaprasetia <221910846@stis.ac.id>
2023-05-28 21:40:48 +0700

    ubah bentar, tapi balik lagi sih, cuma tambahin fungsi round untuk presensi

--- app/Views/page/beranda.php
+++ app/Views/page/beranda.php
@@ -103,7 +103,7 @@
                     <div class="card-body p-0 pb-2">
                         <div class="row">
                             <div class="col-12 text-center">
-                                <input type="text" class="knob" value="<?php echo $presensi; ?>" data-skin="tron" data-thickness="0.2" data-width="200" data-height="200" data-fgColor="#00c0ef" readonly>
+                                <input type="text" class="knob" value="<?php echo round($presensi); ?>" data-skin="tron" data-thickness="0.2" data-width="200" data-height="200" data-fgColor="#00c0ef" readonly>
 
                                 <div class="knob-label"><a href="<?php echo base_url('daftarHadir'); ?>" class="btn btn-secondary">Selengkapnya</a></div>
                             </div>
